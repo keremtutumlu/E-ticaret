4460f5b2d2944a97ebdc952d3f41c366ca5bbd26
keremtutumlu <keremtutumlu1.pg@gmail.com>
2026-06-16 21:30:09 +0300

    Removed duplicated folder

--- eticaret/server/routes/mail.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { sendAcceptCreateMail, sendRejectCreateMail } from "../mail/sellerMail.js";
-
-export const router = Router();
-
-router.post('/sendAcceptMail', sendAcceptCreateMail)
-router.post('/sendRejectMail', sendRejectCreateMail)
